cfc23f3f1bdb05c23258be79092fe3fef8b184d6
EverittGill <eg.everittgill@gmail.com>
2023-06-24 22:44:13 -0400

    got the typeDefs good enough to run graphql at localhost:3001. it isn't opening in apollo though

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,50 +1,47 @@
-const { gql } = require('apollo-server-express')
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
- type User {
+  type User {
     _id: ID
     username: String!
     email: String!
     bookCount: Int
     savedBooks: [Book]
- }
+  }
 
- type Book {
+  type Book {
     bookId: String
-    authors: [""]!
+    authors: [String]!
     description: String
     title: String!
     image: String
     link: String
- }
+  }
 
- type Auth {
+  type Auth {
     token: ID!
-    user: User}
+    user: User
+  }
 
+  type Query {
+    me: User
+  }
 
+  input SaveBookInput {
+    authors: [String]
+    description: String
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
 
- type Query {
-    me: user
-}
-
-type SaveBookMutationInput {
-      authors: [""]
-      description: String
-      title: String!
-      bookId: String!
-      image: String
-      link: String
-}
-
-
-type Mutation {
+  type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(criteria: SaveBookMutationInput): User
+    saveBook(input: SaveBookInput): User
     removeBook(bookId: String!): User
-}
-`
+  }
+`;
 
-
-module.exports = typeDefs
+module.exports = typeDefs;
