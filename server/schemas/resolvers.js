2671a1bad8ab5953f00d396b882c9f1bfc2f227e
EverittGill <eg.everittgill@gmail.com>
2023-06-24 21:58:56 -0400

    resolvers first draft are done

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,48 @@
+// this is the resolvers file and it is where we will create the functionality for each query type we define in typeDefs.js.
+
+// const { AuthenticationError } = require('apollo-server-express');
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+// there will be one for query
+// there will be four for mutation
+
+const resolvers = {
+  Query: {
+    me: async (parent, { args }) => {
+      return User.findOne({ _id: args._id });
+    },
+  },
+  Mutations: {
+    creeateUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const correctPw = await User.isCorrectPassword(body.password);
+      if (!correctPw) {
+        return res.status(400).json({ message: "Wrong password!" });
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { user, body }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $addToSet: { savedBooks: body } },
+        { new: true, runValidators: true }
+      );
+      return res.json(updatedUser);
+    },
+
+    removeBook: async (parent, { user, params }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+    },
+  },
+};
