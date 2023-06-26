c1a5879e16cf5089a83260b051a387def4d8ffad
EverittGill <eg.everittgill@gmail.com>
2023-06-26 14:29:51 -0400

    pushing for askbcs

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+// const { authMiddleware } = require('./utils/auth');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
